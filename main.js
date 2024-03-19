fcecd9be9ca266094d142dfd5916fd12fae43cb5
Guilherme Cursino <guilherme.f96@outlook.com>
2024-03-19 05:37:35 -0300

    Primeiro protótipo do relogio; adicionado novos audios

--- main.js
+++ main.js
@@ -1,46 +1,54 @@
-let listaDeContextos = document.querySelectorAll('.app__card-button');
+let contextoAtual = document.querySelector('html').dataset;
+let seletorDeContextos = document.querySelectorAll('.app__card-button');
+const nomeContexto = [
+    {nome:'foco', tempo: 15},
+    {nome:'descanso-curto', tempo: 3},
+    {nome:'descanso-longo', tempo: 9}
+];
 let imgPrincipal = document.querySelector('.app__image');
 let relogio = document.getElementById('timer');
 let checkBoxMusica = document.getElementById('alternar-musica');
 let buttonComecarPausar = document.querySelector('.app__card-primary-button'); 
-let situacao = buttonComecarPausar.innerText;
-let nomeDoArquivoImg = {Pausar: 'pause', Começar: 'play_arrow'};
+let situacaoStartPause = buttonComecarPausar.innerText;
+const nomeDoArquivoImg = { Pausar: 'pause', Começar: 'play_arrow' };
+const musica = {
+    play: new Audio('/sons/play.wav'),
+    pause: new Audio('/sons/pause.mp3'),
+    beep: new Audio('/sons/beep.mp3'),
+    luna: new Audio('sons/luna-rise-part-one.mp3')
+};
+musica['luna'].loop = true; // Musica toca em loop
 
-const musica = new Audio('sons/luna-rise-part-one.mp3');
-musica.loop = true; // Musica toca em loop
-
+// Configurações iniciais -----------------------------------
+relogio.textContent = nomeContexto[0]['tempo'];
 // Funções --------------------------------------------------
 
-function selecionaContexto(indiceListContexto){
-    listaDeContextos.forEach( function(contexto) {
+function selecionaContexto(indiceLista){
+    seletorDeContextos.forEach( function(contexto) {
         contexto.classList.remove('active');
     });
     
-    listaDeContextos[indiceListContexto].classList.add('active');
+    seletorDeContextos[indiceLista].classList.add('active');
+    contextoAtual.contexto = nomeContexto[indiceLista]['nome'];
 }
 
-function alteraContexto(indiceListContexto) {
-    if (indiceListContexto == 0) {
-        // Foco
-        mudaImgEFundo('foco');
+function alteraContexto(indiceLista) {
+    if (indiceLista == 0) {
         mudaTextoH1('Otimize sua produtividade,', 'mergulhe no que importa.');
 
-    } else if (indiceListContexto == 1) {
-        // Curto
-        mudaImgEFundo('descanso-curto');
+    } else if (indiceLista == 1) {
         mudaTextoH1('Que tal dar uma respirada?', 'Faça uma pausa curta!');
 
-    } else if (indiceListContexto == 2) {
-        // Longo
-        mudaImgEFundo('descanso-longo');
+    } else if (indiceLista == 2) {
         mudaTextoH1('Hora de voltar à superfície.', 'Faça uma pausa longa.');
-
-    }else{
     }
+
+    mudaImgEFundo(contextoAtual.contexto);
+    relogio.textContent = nomeContexto[indiceLista]['tempo'];
 }
 
 function mudaImgEFundo(nomeDataContexto) {
-    document.querySelector('html').dataset.contexto = nomeDataContexto;
+    contextoAtual.contexto = nomeDataContexto;
     imgPrincipal.setAttribute('src', `imagens/${nomeDataContexto}.png`);
 
 /* Outra forma usando Switch no lugar da function mudaTextoH1
@@ -49,8 +57,8 @@ function mudaImgEFundo(nomeDataContexto) {
             document.querySelector('h1').innerHTML =
                 `Otimize sua produtividade,<br> 
                 <strong class="app__title-strong">mergulhe no que importa.</strong>`
-            break;
-
+                break;
+                
         case 'descanso-curto':
             document.querySelector('h1').innerHTML =
                 `Que tal dar uma respirada?<br> 
@@ -71,34 +79,34 @@ function mudaImgEFundo(nomeDataContexto) {
 
 function mudaTextoH1(TEXTO1, TEXTO2) {
     document.querySelector('h1').innerHTML =
-        `${TEXTO1}<br>
-        <strong class="app__title-strong">${TEXTO2}</strong>`
+    `${TEXTO1}<br>
+    <strong class="app__title-strong">${TEXTO2}</strong>`
     ;
 }
 
 function musicaAtivaDesativa(){
-    if(checkBoxMusica.checked && situacao == 'Pausar'){
-        musica.play();
+    if(checkBoxMusica.checked && situacaoStartPause == 'Pausar'){
+        musica['luna'].play();
     }else{
-        musica.pause();
+        musica['luna'].pause();
     }
 }
 
 function trocaButtonComecarPausar(nomeArquivo){
     document.querySelector('.app__card-primary-button-icon').setAttribute('src', `/imagens/${nomeArquivo}.png` );
-    document.querySelector('.app__card-primary-button span').textContent = `${situacao}`;
+    document.querySelector('.app__card-primary-button span').textContent = `${situacaoStartPause}`;
 }
 
 // Eventos ------------------------------------------------------
 
-for (let i = 0; i < listaDeContextos.length; i++) {
-    listaDeContextos[i].onclick = function () {
+for (let i = 0; i < seletorDeContextos.length; i++) {
+    seletorDeContextos[i].onclick = function () {
         selecionaContexto(i);
         alteraContexto(i);
-
-        situacao = 'Começar';
+        
+        situacaoStartPause = 'Começar';
         musicaAtivaDesativa();
-        trocaButtonComecarPausar(nomeDoArquivoImg[situacao]);
+        trocaButtonComecarPausar(nomeDoArquivoImg[situacaoStartPause]);
     }
 }
 
@@ -107,31 +115,45 @@ checkBoxMusica.addEventListener('change', () => {
 });
 
 buttonComecarPausar.addEventListener('click', () => { 
-    situacao = situacao == 'Começar'? 'Pausar': 'Começar';
+    situacaoStartPause = situacaoStartPause == 'Começar'? 'Pausar': 'Começar';
     
     musicaAtivaDesativa();
-    trocaButtonComecarPausar(nomeDoArquivoImg[situacao]);
+    trocaButtonComecarPausar(nomeDoArquivoImg[situacaoStartPause]);
+
+    cronIniciarPausar();
 });
 
 
 // pendencias ----------------------------------------
-    
-// Criar Relogio  
-/*
-    let segundos = 500;
-    relogio.textContent=`${segundos}`;
-    let crons;
 
-    function teste2(){
-        crons = setInterval(() => {
-            seiLa();
-        }, 1000);
-    }
+// Criar Relogio ------------------------------------- 
+let crons = null;
+let tempo ;
 
-    function seiLa(){
-        segundos--;
-        relogio.textContent=`${segundos}`;
-        
+function cronIniciarPausar(){
+    tempo = relogio.textContent;
+    if(crons){
+        // musica['pause'].play();  // som Pausar temporizador
+        cronZerar();
+        return;
+    }else{
+        // musica['play'].play(); // som inicio temporizador 
+        crons = setInterval(()=>{contagem()}, 1000);
     }
-*/
+}
+function cronZerar(){
+    clearInterval(crons);
+    crons = null;
+}
+function contagem(){
+    if (tempo>=0){
+        relogio.textContent = tempo;
+        tempo--;
+    }else{
+        // musica['beep'].play(); // som de tempo esgotado 
+        cronZerar();
+        return;
+    }
+}
+
 
